dc7ef16bb85e4e84599cf722a0d42b7382715b4c
Tom Augspurger <tom.augspurger88@gmail.com>
2016-09-22 09:08:30 -0500

    PKG: Use setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from os import path
+
+import versioneer
+
+
+here = path.abspath(path.dirname(__file__))
+
+# with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+setup(
+    name='sktransformers',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+
+    description='SKTransformers',
+    long_description='SKTransformers',
+
+    url='https://github.com/tomaugspurger/sktransformers',
+
+    author='Tom Augspurger',
+    author_email='tom.augspurger88@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    # keywords='sample setuptools development',
+    packages=find_packages(exclude=['docs', 'tests']),
+
+    install_requires=[],
+
+    extras_require={
+        'dev': [],
+    },
+)
--- sktransformers/__init__.py
+++ sktransformers/__init__.py
@@ -1,3 +1,5 @@
 """A collection of scikit-learn transformers"""
 
-__version__ = '0.0.5'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
